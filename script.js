9696108fecae252c88545f3110567c50c18a6e92
englishle86 <englishle86@gmail.com>
2023-07-07 15:15:38 -0400

    get parseButtonId working to return size to gridGenerator function

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-const row = document.createElement('row')
-const square = document.createElement('div');
+function gridGenerator(button_id){
+    let size = parseButtonId(button_id);
+    console.log(size);
+    //const row = document.createElement('row')
+    //const square = document.createElement('div');
+    // use the button Id to generate a grid
+}
+
+function parseButtonId(button_id){
+    //needs to convert the Button ID into an integer using slice and parseInt
+    let num = button_id.slice(1, );
+    return Number(num);
+    
+}
 
 // event listener to all buttons
 btns = document.querySelectorAll('button');
-console.log(btns);
 
 for (button of btns){
     button.addEventListener('click', function (){
-        alert(button.id)
+        let btnidentity = button.id;
+        gridGenerator(btnidentity);
     })
 }
 
+//on click the button function should call a function which gets  the id of the button and passes it into a gridGenerator function.
 
